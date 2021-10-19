62a3577d64381238da888a9817d1c70136295181
Chukwudi Eze <chukwudieze97@gmail.com>
2021-10-20 00:01:18 +0100

    about to push to remote server

--- components/About.jsx
+++ components/About.jsx
@@ -20,10 +20,10 @@ export default function About() {
         </div>
         <div className="line"></div>
         <div className="text-wrapper">
-          <span>W</span>e are dolor sit amet, consectetur adipisicing elit, sed
+          We are dolor sit amet, consectetur adipisicing elit, sed
           do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          aliquip ex ea commodo consequat. <br /><br /> Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
